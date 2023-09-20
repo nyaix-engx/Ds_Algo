035c439ae35f329c84057ecc1e40632b61c26b44
Sanjay Sajwan <nyaixengnx@Sanjays-MacBook-Air.local>
2023-09-20 08:56:56 +0100

    Added subsequence variation problems

--- Array_Subsequences_Sum.js
+++ Array_Subsequences_Sum.js
@@ -1,24 +1,53 @@
 // #Printing subsequences whose sum is K
 
-const sum1=(array)=>{
-    let result = array.reduce((a, b) => {
-        return a + b;
-      }, 0);
-    return result;
-}
-
-const SumSubsequence=(arr1,arr2,i,target)=>{
-  if(i>=arr2.length){
-    if(sum1(arr1)===target){
-        console.log(arr1);
+const subsequence=(i, arr,target, sum)=>{
+    if(i===target.length){
+        if(sum===2) console.log(arr);
+        return;
     }
-    return;
-  }
-
-  SumSubsequence(arr1.concat(arr2[i]), arr2, i+1, target);
-  SumSubsequence(arr1.slice(0,i),arr2,i+1,target);
+    sum+=target[i];
+    subsequence(i+1,arr.concat(target[i]),target, sum);
+    sum-=target[i];
+    subsequence(i+1, arr.slice(0,i),target, sum);
 }
 
-SumSubsequence([],[5,2,6,2,6,1,8,2,3],0,11)
+
+// subsequence(0, [],[1,2,1] ,0)
 
 
+// #Print any one subsequence whose sum is given to you.
+
+
+const oneSubsequence=(i, arr1, arr2, sum, target)=>{
+    if(i===arr2.length){
+        if(sum===target){
+            console.log(arr1);
+            return true;
+        } 
+        return false;
+    }
+    sum+=arr2[i];
+    if(oneSubsequence(i+1, arr1.concat(arr2[i]),arr2, sum, target)===true) return true;
+    sum-=arr2[i];
+    if(oneSubsequence(i+1, arr1.slice(0,i),arr2, sum, target)===true) return true;
+    return false;
+}
+
+// oneSubsequence(0,[],[1,2,1],0,2)
+
+
+const countSubsequence=(i, arr1, arr2, sum, target, count )=>{
+    if(i===arr2.length){
+        if(sum===target) {
+            return 1
+        }
+        return 0;
+    }
+    sum+=arr2[i];
+    const l = countSubsequence(i+1, arr1.concat(arr2[i]), arr2, sum, target, count );
+    sum-=arr2[i];
+    const r = countSubsequence(i+1, arr1.slice(0,i),arr2, sum, target, count);
+    return l + r;
+}
+
+console.log(countSubsequence(0, [], [1,2,1], 0, 2, 0));
