214f38c3c42386dc89886ada075aed4a81a5f66e
Sanjay Sajwan <nyaixengnx@Sanjays-MacBook-Air.local>
2023-09-23 08:28:45 +0100

    Added combination sum 2 - recusrsion using second way

--- Leetcode/CombinationSum(40).js
+++ Leetcode/CombinationSum(40).js
@@ -1,3 +1,5 @@
+// #Using Recursion - way 1
+
 const backtrack=(index, target, array1, array2, result, map )=>{
     if(index===array2.length){
         if(target===0){
@@ -20,4 +22,32 @@ const combinationSum=(candidates, target)=>{
     return result;
 }
 
-combinationSum([10,1,2,7,6,1,5], 8)
+// combinationSum([10,1,2,7,6,1,5], 8)
+
+
+// #using recursion - way 2
+
+const backtrack2=(index, arr, target, candidates, ds)=>{
+    if(target===0){
+        ds.push([...arr]);
+        return;
+    }
+
+    for(let i=index;i<candidates.length;i++){
+        if(candidates[i]>target) break;
+        if(i>index && candidates[i]===candidates[i-1]) continue;
+        arr.push(candidates[i]);
+        backtrack2(i+1, arr, target-candidates[i], candidates, ds);
+        arr.pop();
+    }
+}
+
+
+const combinationSum2=(target, candidates)=>{
+    const ds=[]
+    backtrack2(0, [], target, candidates.sort((a,b)=>a-b), ds);
+    return ds;
+}
+
+
+console.log(combinationSum2(8, [10,1,2,7,6,1,5] ))
